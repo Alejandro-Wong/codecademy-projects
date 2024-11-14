7d1053fd7bb46371f40cbf8d53f307a6bcaba624
Alejandro_Wong <121511772+Alejandro-Wong@users.noreply.github.com>
2024-11-14 14:27:02 -0500

    removed api key

--- filmFinder/script.js
+++ filmFinder/script.js
@@ -1,8 +1,4 @@
-// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZTdkZjEyNTAyMTU5ZDliYzU3YTc4M2NjZmUxMDIzMCIsIm5iZiI6MTcyNjU5NjIzMS44NjI0OTQsInN1YiI6IjY2ZTljM2M1YjY2NzQ2ZGQ3OTBhZWVmZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eEoDyt4qzn1p794iedCHvg_tmCEIhnxD3hhU54qaRJ4
-// access token
-
-
-const tmdbKey = '6e7df12502159d9bc57a783ccfe10230';
+const tmdbKey = '<API KEY>';
 const tmdbBaseUrl = 'https://api.themoviedb.org/3';
 const playBtn = document.getElementById('playBtn');
 
